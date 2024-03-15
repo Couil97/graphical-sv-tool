8364f61f5450efaf0a8f14ad9b3120a48799e28a
Couil97 <anton.bt@hotmail.com>
2024-03-15 22:54:25 +0100

    Initial Functions

---
+++ index.js
@@ -0,0 +1,349 @@
+let windowHeight;
+let frameArray;
+
+let frameCount = 240;
+let timeScale = 1;
+let stop_interval;
+let draw_stopped = false;
+
+function draw() {
+    frame = 0;
+    
+    let display = document.querySelector('.display');
+
+    let pastHeight = display.scrollHeight;
+
+    let time = Date.now();
+    stop_interval = setInterval(async () => {
+        
+        if(display.scrollHeight != pastHeight) {
+            calculateWindowHeight();
+            calculateHeight(document.querySelector('#svType').value);
+        }
+
+        pastHeight = display.scrollHeight;
+        drawNotes(frame);
+
+        frame++;
+        if(frame >= frameCount) {
+            console.log('Accuracy: ' + (windowHeight / frameArray.at(-1)).toFixed(2));
+            console.log('Elapsed Time: ' + (Date.now() - time));
+
+            time = Date.now();
+            frame = 0;
+        }
+    }, (1000 / frameCount) / timeScale);
+}
+
+function toggleDraw() {
+    if(draw_stopped) {
+        draw();
+        draw_stopped = false;
+    } else {
+        clearInterval(stop_interval);
+        draw_stopped = true;
+    }
+}
+
+function drawNotes(frame) {
+    let maniaNotes = document.querySelectorAll('.mania-note');
+
+    for(let note of maniaNotes){
+        note.style.top = `calc(${frameArray[frame]}px)`;
+    }
+}
+
+function calculateHeight(type) {
+    frameArray = [];
+
+    let min, max, input, res;
+    let j = -1;
+
+    switch(type) {
+        case 'linear':
+            input = parseFloat(document.querySelector('#input').value);
+            for(let i = 0; i < frameCount; i++) {
+                let lastFrame = frameArray[i-1] || 0;
+                frameArray.push((windowHeight / frameCount) * linearSV(i, input) + lastFrame);
+            }
+            break;
+        case 'teleport':
+            min = parseFloat(document.querySelector('#lower').value);
+
+            if(document.querySelector('#urankable').checked) {
+                res = instantTPSV(min);
+                let breakpoint = res.frames;
+                max = res.max;
+
+                for(let i = 0; i < frameCount; i++) {
+                    let lastFrame = frameArray[i-1] || 0;
+    
+                    if(i <= breakpoint) {
+                        frameArray.push((windowHeight / frameCount) * (min * multip) + lastFrame);
+                    } else {
+                        frameArray.push((windowHeight / frameCount) * (max * multip) + lastFrame);
+                    }
+                }
+            } else {
+                max = parseFloat(document.querySelector('#upper').value);
+    
+                res = teleportSV(min, max);
+                let c = 0, c1 = 0, c2 = 0;
+    
+                for(let i = 0; i < frameCount; i++) {
+                    let lastFrame = frameArray[i-1] || 0;
+                    if(i < res.frames) {
+                        frameArray.push((windowHeight / frameCount) * (min * multip) + lastFrame);
+                        c++
+                    } else {
+                        if(i == frameCount - 1 && res.rest != 0) {
+                            frameArray.push((windowHeight / frameCount) * (max * res.rest * multip) + lastFrame);
+                            c1++;
+                        } else {
+                            frameArray.push((windowHeight / frameCount) * (max * multip) + lastFrame);
+                            c2++;
+                        }
+                    }
+                }
+
+                console.log(`Frames: ${res.frames}\nMin Frames: ${c}\nMax Frames: ${c2}\nEnd Frames: ${c1}\nTotal Frames: ${c+c1+c2}\n\nSum: ${(c * min) + (c2 * max) + (c1 * max * res.rest)}`);
+            }
+            break;
+        case 'exponential': 
+            input = parseFloat(document.querySelector('#input').value);
+            res = exponentialSV(input);
+
+            for(let i = 0; i < frameCount; i++) {
+                if(i % (frameCount / 16) == 0) j++;
+
+                let lastFrame = frameArray[i-1] || 0;
+                frameArray.push((windowHeight / frameCount) * res[j] + lastFrame);
+            }
+            break;
+        case 'stutter':
+            max = parseFloat(document.querySelector('#input').value);
+            let freq = parseInt(document.querySelector('#frequency').value);
+
+            min = 1.11111111111 - (max / 9);
+            let length = Math.round(frameCount / freq); 
+            c = 0;
+
+            let c1 = 0, c2 = 0, c3 = 0;
+
+            let lengthArr = [];
+            let sum = 0;
+
+            let above_half = false;
+
+            let rest = (length % 1);
+            if(rest > .5) {
+                rest = 1 - rest;
+                above_half = true;
+            }
+            rest = Math.round(1/rest);
+            let currentLength = 0;
+
+            for(let i = 0; i < freq; i++) {
+                if(!above_half){
+                    if(i % rest == 0) {
+                        sum += Math.ceil(length)
+                        currentLength = Math.ceil(length);
+                    } else {
+                        sum += Math.floor(length)
+                        currentLength = Math.floor(length);
+                    }
+                } else {
+                    if(i % rest == 0) {
+                        sum += Math.floor(length);
+                        currentLength = Math.floor(length);
+                    } else {
+                        sum += Math.ceil(length);
+                        currentLength = Math.ceil(length);
+                    }
+                }
+
+                lengthArr.push({sum: sum, length: currentLength});
+            }
+
+            res = teleportSV(min, max, lengthArr[0].length);
+            breakpoint = res.frames;
+
+            for(let i = 0; i < frameCount; i++) {
+                let lastFrame = frameArray[i-1] || 0;
+                let idx = lengthArr.findIndex(x => x.sum == i);
+
+                if(idx != -1) {
+                    c++;
+                    res = teleportSV(min, max, lengthArr[idx].length);
+                    breakpoint = i + res.frames;
+                }
+
+                if(i < breakpoint) {
+                    frameArray.push((windowHeight / frameCount) * (min * multip) + lastFrame);
+                    c1++;
+                } else {
+                    if(i == lengthArr[c].sum - 1 && res.rest != 0) {
+                        frameArray.push((windowHeight / frameCount) * (max * res.rest * multip) + lastFrame);
+                        c2++;
+                    } else if(i == frameCount - 1 && res.rest != 0) {
+                        frameArray.push((windowHeight / frameCount) * (max * res.rest * multip) + lastFrame);
+                        c2++;
+                    } else {
+                        frameArray.push((windowHeight / frameCount) * (max * multip) + lastFrame);
+                        c3++;
+                    }
+                }
+            }
+            
+            console.log(`Min Frames: ${c1}\nMax Frames: ${c3}\nEnd Frames: ${c2}\nTotal Frames: ${c1+c2+c3}\n\nSum: ${(c1 * min) + (c3 * max) + (c2 * max * res.rest)}`);
+
+            break;
+        default:
+            for(let i = 0; i < frameCount; i++) {
+                frameArray.push((i * windowHeight) / frameCount);
+            }
+            break;
+    }
+}
+
+function linearSV(frame, end) {
+    let start = 2 - end;
+    let delta = ((end - start) / frameCount);
+
+    if(frameCount % 2 == 0) {
+        if(frame >= (frameCount / 2)) frame++; // Skips a frame since frameCount is even
+    }
+
+    let adj = start + (delta * frame);
+
+    return adj * multip;
+}
+
+function instantTPSV(min) {
+    // frames = 239;
+    // min = 0.5, framesLeft = 120
+    // max = 120
+
+    let framesLeft = frameCount - 1;
+    let max = frameCount - (framesLeft * min);
+
+    return {frames: framesLeft-1, max: max}
+}
+
+function teleportSV(min, max, length = frameCount) {
+    let framesLeft = length;
+    let minFrames = 0, maxFrames = 0, prevMaxFrames;
+    let c = 0;
+    
+    do {
+        minFrames = (framesLeft - maxFrames) * min;
+        maxFrames = Math.ceil((length - minFrames) / max);
+
+        if(maxFrames == prevMaxFrames) break;
+        prevMaxFrames = maxFrames;
+
+        c++
+    } while(c < 10)
+
+    minFrames = (framesLeft - maxFrames) * min;
+    maxFrames = (length - minFrames) / max;
+
+    let frames = length - Math.ceil(maxFrames);
+
+    return { frames: frames, rest: maxFrames % 1 }
+}
+
+function exponentialSV(limit) {
+    let t = new Array(16);
+    let d = 0.1;
+    let change = 0.1;
+    let prevD = [];
+
+    let c = 0;
+
+    while (c < 100) {
+        let initial_value = Math.log(limit);
+        sum = 0;
+
+        for (let i = 0; i < 16; i++) {
+            t[i] = Math.exp(initial_value);
+            initial_value = initial_value + d;
+            sum = sum + t[i];
+        }
+
+        prevD.push(d);
+
+        if ((sum / 16) > (multip + (0.0001 * multip))) d = d - change;
+        else if ((sum / 16) < (multip - (0.0001 * multip))) d = d + change;
+        else break;
+
+        if(d == prevD.at(-2)) change /= 10;
+
+        c++;
+    }
+
+    console.log('EXP retry count: ' + c);
+    console.log('d: ' + change);
+
+    return t;
+}
+
+function renderSVManipulation() {
+    let svManipulationCanvas = document.querySelector('.sv-manipulation');
+    svManipulationCanvas.innerHTML = '';
+
+    calculateWindowHeight();
+
+    switch(document.querySelector('#svType').value) {
+        case 'linear':
+            renderLinear(svManipulationCanvas);
+            break;
+        case 'teleport':
+            renderTeleport(svManipulationCanvas);
+            break;
+        case 'exponential':
+            renderExponential(svManipulationCanvas);
+            break;
+        case 'stutter':
+            renderStutter(svManipulationCanvas);
+            break;
+        case 'stop':
+            renderStop(svManipulationCanvas);
+            break;
+    }
+
+    calculateHeight(document.querySelector('#svType').value);
+}
+
+function calculateWindowHeight() {
+    let display = document.querySelector('.display');
+    let receptor = document.querySelector('.mania-receptor');
+
+    windowHeight = display.scrollHeight - (receptor.offsetHeight / 2) - 20;
+}
+
+function setTimescale(input) {
+    timeScale = parseFloat(input);
+    toggleDraw();
+    toggleDraw();
+}
+
+let multip = document.querySelector('#songBpm').value / document.querySelector('#currentBpm').value;
+multip *= document.querySelector('#currentMulti').value;
+
+document.querySelectorAll('.song-info-inputs').forEach(element => {
+    element.addEventListener('change', function () {
+        multip = document.querySelector('#songBpm').value / document.querySelector('#currentBpm').value;
+        multip *= document.querySelector('#currentMulti').value;
+
+        calculateHeight(document.querySelector('#svType').value);
+    })
+});
+
+document.querySelector('#svType').addEventListener('change', renderSVManipulation);
+renderSVManipulation()
+
+// Delays a async function.
+const delay = async (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms));
+
+draw();
---
+++ modules/render-module.js
@@ -0,0 +1,210 @@
+function renderLinear(canvas) {
+    let times = getTimeInputs();
+    let slider = getInputSlider('SV Limit', 1, 0.01, 2, 1.5, 'input', 'linear');
+
+    canvas.append(times, slider);
+}
+
+function renderTeleport(canvas) {
+    let times = getTimeInputs();
+    let lower = getInputSlider('Lower Boundry', 0.01, 0.01, 1, 0.5, 'lower', 'teleport');
+    let upper = getInputSlider('Upper Boundry', 1, 0.01, 10, 2, 'upper', 'teleport');
+    let instant_tp = getCheckbox('Enable Instant Teleport', 'teleport');
+
+    let input_group = document.createElement('div');
+    input_group.className = 'input-group';
+
+    input_group.append(lower, upper, instant_tp);
+
+    canvas.append(times, input_group);
+}
+
+function renderExponential(canvas) {
+    let times = getTimeInputs();
+    let input = getInputSlider('Intensity', 0.01, 0.01, 10, 1.5, 'input', 'exponential');
+
+    let input_group = document.createElement('div');
+    input_group.className = 'input-group';
+
+    input_group.append(input);
+
+    canvas.append(times, input_group);
+}
+
+function renderStutter(canvas) {
+    let times = getTimeInputs();
+    let input = getInputSlider('Intensity', 1, 0.01, 10, 5, 'input', 'stutter');
+    let frequency = getFrequecySelect();
+
+    let input_group = document.createElement('div');
+    input_group.className = 'input-group';
+
+    input_group.append(input, frequency);
+
+    canvas.append(times, input_group);
+}
+
+function getTimeInputs() {
+    let input_group = document.createElement('div');
+    input_group.className = 'input-group';
+
+    let start_div = document.createElement('div');
+    start_div.className = "input";
+
+    let start_label = document.createElement('label');
+    let start_input = document.createElement('input');
+
+    start_label.textContent = 'Start';
+    start_input.id = 'start';
+
+    start_div.append(start_label, start_input);
+
+    let end_div = document.createElement('div');
+    end_div.className = "input";
+
+    let end_label = document.createElement('label');
+    let end_input = document.createElement('input');
+
+    end_label.textContent = 'End';
+    end_input.id = 'end';
+
+    end_div.append(end_label, end_input);
+
+    input_group.append(start_div, end_div);
+
+    return input_group;
+}
+
+function getInputSlider(name, lowerLimit, step, upperLimit, default_value, id, type) {
+    let div = document.createElement('div');
+    div.className = 'input';
+
+    let slider_label = document.createElement('label');
+    slider_label.textContent = name;
+
+    let slider = document.createElement('input');
+    slider.id = id;
+    slider.type = 'range';
+    slider.min = lowerLimit;
+    slider.step = step;
+    slider.max = upperLimit;
+    slider.value = default_value;
+    slider.oninput = function() { this.nextElementSibling.textContent = this.value; calculateHeight(type); } 
+
+    let output = document.createElement('div');
+    output.className = 'output';
+    output.textContent = default_value;
+    output.contentEditable = true;
+    output.oninput = function() { 
+        if(parseFloat(this.textContent) < lowerLimit && parseFloat(this.textContent) != 0) this.textContent = lowerLimit;
+        if(parseFloat(this.textContent) > upperLimit) this.textContent = upperLimit;
+        this.previousElementSibling.value = this.textContent;
+    }
+
+    div.append(slider_label, slider, output);
+
+    return div;
+}
+
+function getCheckbox(name, svType, id, type) {
+    let div = document.createElement('div');
+    div.className = 'input checkbox';
+
+    let checkbox_label = document.createElement('label');
+    checkbox_label.textContent = name;
+    
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'urankable';
+    checkbox.oninput = function () { calculateHeight(svType); }
+
+    if(type == 'urankable') {
+        checkbox.onchange = function() { toggle_unrankable(id) }
+    }
+
+    div.append(checkbox_label, checkbox);
+
+    return div;
+}
+
+function getFrequecySelect() {
+    let div = document.createElement('div');
+    div.className = 'input frequency';
+
+    let frequency_label = document.createElement('label');
+    frequency_label.textContent = 'Frequency';
+    
+    let frequency = document.createElement('select');
+    frequency.id = 'frequency';
+
+    frequency.oninput = function () { 
+        calculateHeight('stutter');
+    };
+
+    let optionsArray = [2,3,4,6,8,10,12,16,20,24,28,36,44,52,64]; 
+
+    for(let i = 0; i < optionsArray.length; i++) {
+        let option = document.createElement('option');
+        option.value = optionsArray[i];
+        option.textContent = '1/' + optionsArray[i];
+
+        if(i == 7) option.selected = true;
+
+        frequency.append(option);
+    }
+
+    div.append(frequency_label, frequency);
+
+    return div;
+}
+
+function getFrequecyWrite() {
+    let div = document.createElement('div');
+    div.className = 'input frequency';
+
+    let frequency_label = document.createElement('label');
+    frequency_label.textContent = 'Frequency';
+
+    let span = document.createElement('span');
+    
+    let frequency = document.createElement('input');
+    frequency.type = 'text';
+    frequency.value = 2;
+    frequency.id = 'frequency'
+    frequency.style = 'width: 0.5em; font:inherit; padding-left: 1em;';
+    frequency.oninput = function () { 
+        if(this.value.length > 1) this.value = this.value.at(-1);
+        if(isNaN(this.value) || this.value == 0) this.value = '1';
+        calculateHeight('stutter');
+    };
+
+    let text = document.createElement('span');
+    text.textContent = '1/';
+    text.style = 'margin-left:-1.6em; color:black; width: 0; user-select: none; padding-right: 12px;';
+    text.onclick = function () { frequency.focus() };
+
+    span.append(frequency, text);
+
+    div.append(frequency_label, span);
+
+    return div;
+}
+
+function toggle_unrankable(id) {
+    let element = document.querySelector(`#${id}`);
+
+    if(element.max == 10) {
+        element.max = 1000;
+        element.value = Math.round(element.value);
+        element.step = 1;
+    }else {
+        element.max = 10;
+        element.step = 0.01;
+    }
+}
+
+function toggleInstantTP() {
+    let element = document.querySelector('#instantTP');
+    if(element.checked) element.checked = false;
+    else element.checked = true;
+}
