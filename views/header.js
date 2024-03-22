9005eb8e132d38ed9157c0783df1d678891b0fed
Couil97 <anton.bt@hotmail.com>
2024-03-22 23:32:50 +0100

    Changed pathing to work with gh-pages

--- views/header.js
+++ views/header.js
@@ -1,14 +1,16 @@
+let path = document.querySelector('#data-header').dataset.path;
+
 document.write(`
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/style.css">
-        <script src="/modules/dom-module.js" defer></script>
-        <script src="/modules/helper-module.js" defer></script>
-        <script src="/modules/render-module.js" defer></script>
-        <script src="/modules/sv-module.js" defer></script>
-        <script src="/main.js" defer></script>
+        <link rel="stylesheet" href="${path}style.css">
+        <script src="${path}modules/dom-module.js" defer></script>
+        <script src="${path}modules/helper-module.js" defer></script>
+        <script src="${path}modules/render-module.js" defer></script>
+        <script src="${path}modules/sv-module.js" defer></script>
+        <script src="${path}main.js" defer></script>
         <title>Graphical SV Tool</title>
 `);
